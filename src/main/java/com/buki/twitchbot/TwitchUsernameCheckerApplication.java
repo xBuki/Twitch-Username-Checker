2a3745f644ff92c0d7ccd7e68bac1dece5341f73
Edson <70242366+xBuki@users.noreply.github.com>
2025-09-18 14:36:38 -0700

    Initial Commit

---
+++ src/main/java/com/buki/twitchbot/TwitchUsernameCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.buki.twitchbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchUsernameCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchUsernameCheckerApplication.class, args);
+	}
+
+}
